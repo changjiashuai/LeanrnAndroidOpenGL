9599cc8977d20a77f957189c213f5bd2c2e93a66
changjiashuai <rj1031cjs@163.com>
2017-05-11 10:03:33 +0800

    add VertexArrayObject just support in OpenGL 3.0 above.

---
+++ app/src/main/java/io/github/changjiashuai/leanrnandroidopengl/VertexArrayObject.java
@@ -0,0 +1,47 @@
+package io.github.changjiashuai.leanrnandroidopengl;
+
+import android.annotation.TargetApi;
+import android.opengl.GLES30;
+import android.os.Build;
+
+/**
+ * Email: changjiashuai@gmail.com
+ *
+ * Created by CJS on 2017/5/11 09:44.
+ *
+ * //init
+ * createVAO()
+ * bindVBO
+ * unBindVAO
+ *
+ * //use
+ * useProgram()
+ * createVAO()
+ * render()
+ * unBindVAO()
+ *
+ */
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+public class VertexArrayObject {
+
+    private int mVAOHandle = -1;
+
+    public int getVAOHandle() {
+        return mVAOHandle;
+    }
+
+    public void createVAO() {
+        int[] arrays = new int[1];
+        GLES30.glGenVertexArrays(1, arrays, 0);
+        mVAOHandle = arrays[0];
+        GLES30.glBindVertexArray(mVAOHandle);
+        //bind vbo
+        VertexBufferObject vbo = new VertexBufferObject();
+        vbo.create();
+    }
+
+    public void unBindVAO() {
+        GLES30.glBindVertexArray(0);
+    }
+}
